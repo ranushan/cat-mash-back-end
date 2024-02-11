555b302a7b5684f875593499e4378b7325081ed7
Ranushan <ranusha.n@hotmail.fr>
2024-02-11 03:54:11 +0100

    [main][back] adding logging

--- src/main/java/com/ranushan/service/CatServiceImpl.java
+++ src/main/java/com/ranushan/service/CatServiceImpl.java
@@ -2,19 +2,20 @@ package com.ranushan.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ranushan.dao.CatDao;
 import com.ranushan.domain.Cat;
 import com.ranushan.exception.CatJsonUnReadException;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class CatServiceImpl implements CatService {
@@ -48,16 +49,26 @@ public class CatServiceImpl implements CatService {
      * Save all cats from API
      */
     private void saveAllCats() {
+        log.info("==========> START CALL API Atelier ==========");
         var json = restTemplate
                 .getForEntity("https://data.latelier.co/cats.json", String.class)
                 .getBody();
         var reader = mapper.reader().withRootName("images").forType(new TypeReference<List<Cat>>(){});
         try {
             List<Cat> cats = reader.readValue(json);
-            cats.forEach(c -> c.setVotes(0));
-            catDao.saveAll(cats);
+            checkIfCatIsPresent(cats);
+            log.info("========== END CALL API Atelier WITH SAVING <==========");
         } catch (JsonProcessingException e) {
             throw new CatJsonUnReadException("We can't read json file providing by API or DB is not save properly !!!");
         }
+    }
+
+    private void checkIfCatIsPresent(List<Cat> cats) {
+        var allCatPictures = getAllCatPictures();
+        var newCats = cats.stream()
+                .filter(c -> allCatPictures.stream().noneMatch(alp -> c.getId().equals(alp.getId())))
+                .collect(Collectors.toList());
+        newCats.forEach(c -> c.setVotes(0));
+        catDao.saveAll(newCats);
     }
 }
