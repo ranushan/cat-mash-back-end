ee375716f40c68a75df8c82c71b3bf731dd3f9db
Ranushan <ranusha.n@hotmail.fr>
2024-02-11 11:35:29 +0100

    [main][back] adding cross origin

--- src/main/java/com/ranushan/controller/CatController.java
+++ src/main/java/com/ranushan/controller/CatController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@CrossOrigin
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api")
